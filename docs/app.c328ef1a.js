fad34f2f81dff6f797dd702f7fb02db49b095288
Maciej Żelazo <voidereles3000@gmail.com>
2021-02-17 13:18:06 +0100

    dddd

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -22039,7 +22039,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56986" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57063" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
